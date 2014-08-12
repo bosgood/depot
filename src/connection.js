bf1f6b42cce408b584937bef04003fc8ad76bc95
bosgood <bosgood@gmail.com>
2014-08-11 20:50:20 -0500

    Basic Connection interface specced out

--- src/connection.js
+++ src/connection.js
@@ -1,14 +1,71 @@
 /**
- * Abstracts over a database connection
+ * Interface for abstracting over a database connection
  */
-
 function Connection() {
   this.connected = false;
 }
 
-Connection.prototype.connect = function(err, data) {
+/**
+ * Connects to the datastore
+ * @param callback {err, data}
+ */
+Connection.prototype.connect = function(callback) {
   this.connected = true;
   return this;
+};
+
+/**
+ * Disconnects from the datastore
+ * @param callback {err, data}
+ */
+Connection.prototype.disconnect = function(callback) {
+  this.connected = false;
+  return this;
+};
+
+/**
+ * Gets the latest version contents
+ * @param callback {err, data}
+ */
+Connection.prototype.getLatest = function(callback) {
+};
+
+/**
+ * Gets the version contents for a specific id
+ * @param {string} versionId
+ * @param callback {err, data}
+ */
+Connection.prototype.getVersion = function(versionId, callback) {
+};
+
+/**
+ * Gets the latest deployIds
+ * @param {integer} limit (defaults to 20)
+ * @param callback {err, data}
+ */
+Connection.prototype.getVersions = function(limit, callback) {
+  if (limit <= 0) {
+    limit = 20;
+  }
+};
+
+/**
+ * Gets the deployable applications
+ * @param {integer} limit (defaults to 20)
+ * @param callback {err, data}
+ */
+Connection.prototype.getApplications = function(limit, callback) {
+  if (limit <= 0) {
+    limit = 20;
+  }
+};
+
+/**
+ * Gets a specific application information object
+ * @param {string} applicationId
+ * @param callback {err, data}
+ */
+Connection.prototype.getApplication = function(applicationId, callback) {
 };
 
 module.exports = Connection;
