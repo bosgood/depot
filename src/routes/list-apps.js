53c06f8b9d2899c2aced3f419e2e4d4b4983666f
bosgood <bosgood@gmail.com>
2014-08-13 17:00:11 -0500

    Basic list apps endpoint

--- src/routes/list-apps.js
+++ src/routes/list-apps.js
@@ -1,5 +1,12 @@
 var route = function(connection) {
   return function listApps(req, res) {
+    connection.getApplications(null, null, function(err, data) {
+      if (err) {
+        res.status(500).send({ error: err.toString() });
+        return;
+      }
+      res.status(200).send(data);
+    });
   };
 }
 
